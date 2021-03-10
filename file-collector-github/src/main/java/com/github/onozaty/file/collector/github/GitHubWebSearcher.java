835842ab8c496163050b42208e17c1bbc36db312
onozaty <onozaty@gmail.com>
2021-03-10 23:12:11 +0900

    [GitHub] 複数クエリ指定可能な形に修正

--- file-collector-github/src/main/java/com/github/onozaty/file/collector/github/GitHubWebSearcher.java
+++ file-collector-github/src/main/java/com/github/onozaty/file/collector/github/GitHubWebSearcher.java
@@ -4,6 +4,7 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,11 +15,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * GitHubのWeb上で検索するクラスです。
  * @author onozaty
  */
+@Slf4j
 public class GitHubWebSearcher {
 
     /**
@@ -31,42 +34,77 @@ public class GitHubWebSearcher {
 
     /**
      * 指定クエリでGitHubのコード検索を行い、検索結果からURLを取得します。
-     * @param query 検索クエリ
+     * @param queries 検索クエリ一覧
      * @return URL一覧
      * @throws Exception
      */
-    public List<String> search(String query) throws Exception {
+    public List<String> search(List<String> queries) throws Exception {
 
         WebDriver driver = new ChromeDriver();
 
         try {
             List<String> resultUrls = new ArrayList<>();
 
-            driver.get(
-                    "https://github.com/search?type=code&q="
-                            + URLEncoder.encode(query, StandardCharsets.UTF_8.name()));
+            for (int i = 0; i < queries.size(); i++) {
+                String query = queries.get(i);
 
-            // ログインが済むまで待ち合わせ
-            new WebDriverWait(driver, Duration.ofMinutes(2).getSeconds())
-                    .until(ExpectedConditions.urlContains("https://github.com/search?"));
+                try {
+                    log.info("Search started. query({}/{})=[{}]", i + 1, queries.size(), query);
+                    List<String> urls = search(driver, query);
+                    log.info("Search finished. count=[{}]", urls.size());
 
-            delay();
-            resultUrls.addAll(collectResultUrls(driver));
-
-            // 次ページのリンクがなくなるまで繰り返し
-            while (!driver.findElements(By.cssSelector("a.next_page")).isEmpty()) {
-
-                driver.findElement(By.cssSelector("a.next_page")).click();
-
-                delay();
-                resultUrls.addAll(collectResultUrls(driver));
+                    resultUrls.addAll(urls);
+                } catch (Exception e) {
+                    log.error("An error occurred in the search.", e);
+                }
             }
 
-            return resultUrls;
+            // 複数クエリの場合、重複するURLが存在する可能性があるため
+            return resultUrls.stream()
+                    .distinct()
+                    .collect(Collectors.toList());
 
         } finally {
             driver.quit();
         }
+    }
+
+    /**
+     * 指定クエリでGitHubのコード検索を行い、検索結果からURLを取得します。
+     * @param query 検索クエリ
+     * @return URL一覧
+     * @throws Exception
+     */
+    public List<String> search(String query) throws Exception {
+
+        return search(Arrays.asList(query));
+    }
+
+    private List<String> search(WebDriver driver, String query) throws Exception {
+
+        List<String> resultUrls = new ArrayList<>();
+
+        driver.get(
+                "https://github.com/search?type=code&q="
+                        + URLEncoder.encode(query, StandardCharsets.UTF_8.name()));
+
+        // ログインが済むまで待ち合わせ
+        new WebDriverWait(driver, Duration.ofMinutes(2).getSeconds())
+                .until(ExpectedConditions.urlContains("https://github.com/search?"));
+
+        delay();
+        resultUrls.addAll(collectResultUrls(driver));
+
+        // 次ページのリンクがなくなるまで繰り返し
+        while (!driver.findElements(By.cssSelector("a.next_page")).isEmpty()) {
+
+            driver.findElement(By.cssSelector("a.next_page")).click();
+
+            delay();
+            resultUrls.addAll(collectResultUrls(driver));
+        }
+
+        return resultUrls;
     }
 
     private void delay() throws InterruptedException {
--- file-collector-github/src/test/java/com/github/onozaty/file/collector/github/GitHubWebSearcherTest.java
+++ file-collector-github/src/test/java/com/github/onozaty/file/collector/github/GitHubWebSearcherTest.java
@@ -2,6 +2,7 @@ package com.github.onozaty.file.collector.github;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -22,11 +23,26 @@ public class GitHubWebSearcherTest {
 
         GitHubWebSearcher searcher = new GitHubWebSearcher();
 
-        List<String> urls = searcher.search("extension:pdf filename:test");
+        List<String> urls = searcher.search("extension:pdf filename:test5");
 
         assertThat(urls)
                 .hasSizeGreaterThan(100);
+    }
 
+    /**
+     * {@link GitHubWebSearcher#search(List)}のテストです。
+     * @throws Exception
+     */
+    @Test
+    public void search_複数クエリ() throws Exception {
+
+        GitHubWebSearcher searcher = new GitHubWebSearcher();
+
+        List<String> urls = searcher.search(
+                Arrays.asList("extension:pdf filename:test5", "extension:eml filename:mail"));
+
+        assertThat(urls)
+                .hasSizeGreaterThan(500);
     }
 
 }
